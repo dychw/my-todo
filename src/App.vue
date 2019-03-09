2ecd84bd6a8ee1916448b04f7f36a5a375b2d061
dychw <25680873+dychw@users.noreply.github.com>
2019-03-10 08:30:36 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDoリスト</h1>
+    <TodoList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input type="radio" id="one" value="One" v-model="picked">全て
+    <input type="radio" id="one" value="One" v-model="picked">作業中
+    <input type="radio" id="one" value="One" v-model="picked">完了
+    <table>
+      <tbody>
+        <th>ID</th>
+        <th>コメント</th>
+        <th colspan="2">状態</th>
+        <tr v-for="todo in todos" v-bind:key="todo.id">
+          <td>{{ todo.id }}</td>
+          <td>{{ todo.comment }}</td>
+          <td>{{ todo.state }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>新規タスクの追加</h2>
+    <input type="text" class="input" v-model="inputTodo">
+    <button @click="addTodo">追加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      inputTodo: "",
+      todos: [{ id:0,comment: "def",state:"b" }]
+    };
+  },
+  methods: {
+    addTodo: function() {
+      this.todos.push({ title: this.inputTodo });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
